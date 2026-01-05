4b255644f3c1a7f6c05c9a88b144f8a03f53c6dc
jaxsonsuth <jacksonsuth@gmail.com>
2026-01-05 00:59:27 -0800

    gitignore:

--- server/target/debug/deps/sprout_server-79fa75162c867ab2.d
+++
@@ -1,5 +0,0 @@
-/Users/jacksonsutherland/Projects/sprout.ai/server/target/debug/deps/sprout_server-79fa75162c867ab2.d: src/main.rs
-
-/Users/jacksonsutherland/Projects/sprout.ai/server/target/debug/deps/libsprout_server-79fa75162c867ab2.rmeta: src/main.rs
-
-src/main.rs:
